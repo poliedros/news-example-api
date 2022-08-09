d2239e13f5455cd9e5d63168d41c6abede7e1d15
Carlos <carloshlzansavio@gmail.com>
2022-08-09 13:27:50 +0100

    feat: add news list

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type NewsDto = {
+export type NewsDto = {
   headline: string;
   publishedAt: Date;
   by: string;
-  image_url: string;
 };
 
 export default function handler(
@@ -18,16 +17,12 @@ export default function handler(
         "Is iPhone 14 release date set to be earlier than expected? - The National",
       publishedAt: new Date(),
       by: "BBC",
-      image_url:
-        "https://news.google.com/__i/rss/rd/articles/CBMiZGh0dHBzOi8vd3d3LnJ0ZS5pZS9lbnRlcnRhaW5tZW50LzIwMjIvMDgwOC8xMzE0NjU0LWpvaG4tdHJhdm9sdGEtcGF5cy10cmlidXRlLXRvLW9saXZpYS1uZXd0b24tam9obi_SAQA?oc=5",
     },
     {
       headline:
         "John Travolta among stars to pay tribute to Newton-John: 'You made all of our lives so much better' - RTE.ie",
       publishedAt: new Date(),
       by: "Google News",
-      image_url:
-        "https://news.google.com/__i/rss/rd/articles/CBMiZGh0dHBzOi8vd3d3LnJ0ZS5pZS9lbnRlcnRhaW5tZW50LzIwMjIvMDgwOC8xMzE0NjU0LWpvaG4tdHJhdm9sdGEtcGF5cy10cmlidXRlLXRvLW9saXZpYS1uZXd0b24tam9obi_SAQA?oc=5",
     },
   ]);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import type { NextPage } from "next";
+import useSWR from "swr";
+import { NewsDto } from "./api/news";
 
 const Home: NextPage = () => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error } = useSWR<NewsDto[]>("/api/news", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  const newsList = data.map((newDto) => {
+    return (
+      <li key={newDto.headline}>
+        <p>{newDto.headline}</p>
+        <div>
+          By: {newDto.by} Published at: {newDto.publishedAt}
+        </div>
+        <hr />
+      </li>
+    );
+  });
+
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline">hi</h1>
+    <div className="container mx-auto flex">
+      <ul className="space-y-4">{newsList}</ul>
     </div>
   );
 };
